fa871b13e693de26f5545985e68ed8f824c51c0d
Turner Bohlen <turnerbohlen@gmail.com>
2014-01-28 00:57:00 -0500

    Adds stream operators for building

--- src/model/building.cpp
+++ src/model/building.cpp
@@ -1,6 +1,10 @@
 #include "building.hpp"
 #include "math.h"
 #include "types.h"
+#include <cassert>
+#include <string>
+
+using namespace std;
 
 Building::Building(boolMatrix exists, float xSize, float ySize, float zSize) {
 	this->exists = exists;
@@ -38,9 +42,9 @@ bool Building::checkIntersect(indices const panel, coord const sundir) const {
 		py = c.y - sundir.y*t;
 		pz = c.z + dz;
 		//find indices of panel associated with intersection point
- 		pxind = floor(px/(this->xSize));
- 		pyind = floor(py/(this->ySize));
- 		pzind = floor(pz/(this->zSize));
+		pxind = floor(px/(this->xSize));
+		pyind = floor(py/(this->ySize));
+		pzind = floor(pz/(this->zSize));
 		//check if panel exists at this location
 		if (this->exists[pxind][pyind][pzind] == 1) {
 			shade = 1;
@@ -49,3 +53,91 @@ bool Building::checkIntersect(indices const panel, coord const sundir) const {
 	}
 	return shade;
 }
+
+int Building::getSize() {
+    return this->exists.size();
+}
+int Building::getSecondSize(int i) {
+    assert(i < this->exists.size());
+    return this->exists[i].size();
+}
+
+int Building::getThirdSize(int i, int j) {
+    assert(i < this->exists.size());
+    assert(j < this->exists[i].size());
+    return this->exists[i][j].size();
+}
+
+bool Building::floorExists(int i, int j, int k) {
+    assert(i >= 0);
+    assert(i < this->exists.size());
+    assert(j >= 0);
+    assert(j < this->exists[i].size());
+    assert(k >= 0);
+    assert(k < this->exists[i][j].size());
+    return this->exists[i][j][k];
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Operator overloading
+////////////////////////////////////////////////////////////////////////////////
+
+ostream& operator << (ostream& os, const Building& b) {
+    // save the first size
+    int i, j, k;
+    int size = b.getSize();
+    os << "Size: " << size << endl;
+    for (i = 0; i < size; i++) {
+        int secondSize = b.getSecondSize(i);
+        os << "nextX: " << secondSize << end;
+        for (j = 0; j < secondSize; j++) {
+            int thirdSize = b.getThirdSize(i, j);
+            os << "nextY: " << thirdSize << endl;
+            for (k = 0; k < thirdSize; k++) {
+                os << "panel: " << b.exists(i, j, k) << endl;
+            }
+        }
+    }
+    os << "end" << endl;
+}
+
+istream& operator >> (istream& os, const Building& b) {
+    int i = -1;
+    int j = -1;
+    int k = -1;
+
+    string str;
+    os >> str;
+    assert(str.compare("Size:") == 0);
+
+    int size;
+    os >> size;
+    b.exists.resize(size);
+    while (os >> str) {
+        // read through each word
+        if (str.compare("panel:") == 0) {
+            // load the next panel
+            k++;
+            os >> b.exists[i][j][k];
+        }
+        else if (str.compare("nextX:") == 0) {
+            // load the size of the next x array
+            i++;
+            int xSize;
+            os >> xSize;
+            b.exists[i].resize(size);
+        }
+        else if (str.compare("nextY:") == 0) {
+            // load the size of the next y array
+            j++;
+            int ySize;
+            os >> ySize;
+            b.exists[i][j].resize(ySize);
+        }
+        else if (str.compare("end") == 0) {
+            // exit
+            break;
+        }
+    }
+}
+
--- src/model/building.hpp
+++ src/model/building.hpp
@@ -2,6 +2,9 @@
 #define BUILDING_H
 
 #include "types.h"
+#include <iostream>
+
+#define MAX_PARSER_TOKEN_LENGTH 100
 
 /*
  * Class Building
@@ -10,7 +13,7 @@
  * boolean value determining existance of panel.
  */
 
-class Building { 
+class Building {
 	public:
 		/*
 		 * Constructor: Building
@@ -18,9 +21,9 @@ class Building {
 		Building(boolMatrix exists, float xSize, float ySize, float zSize);
 
 		~Building();
-		
+
 		/*
-		 * Method inShade
+		 * Method: inShade
 		 *
 		 * Determines if ray from centroid to the sun hits another panel,
 		 * thereby putting it in shade.
@@ -34,7 +37,36 @@ class Building {
 		 * 	True: hits panel, panel is in shade
 		 * 	False: no panel intersects with ray, panel is in sunlight
 		 */
-		bool inShade(indices const panel, coord const sundir) const; 
+		bool inShade(indices const panel, coord const sundir) const;
+
+        /*
+         * Method: getSize
+         *
+         * Returns the size of the top-level dimension of the boolMatrix.
+         */
+        int getSize();
+
+        /*
+         * Method: getSecondSize
+         *
+         * Returns the size of the ith row of the exists matrix.
+         */
+        int getSecondSize(int i);
+
+        /*
+         * Method: getThirdSize
+         *
+         * Returns the size of the jth column of the ith row of the exists
+         * matrix.
+         */
+        int getThirdSize(int i, int j);
+
+        /*
+         * Method: floorExists
+         *
+         * Returns true if there is a floor panel at i, j, k and false if not.
+         */
+        bool floorExists(int i, int j, int k);
 
 	private:
 		boolMatrix exists;
@@ -42,7 +74,23 @@ class Building {
 		float ySize;
 		float zSize;
 		coordMatrix centroids;
-		
+
 };
+
+/*
+ * Operator: <<
+ * 
+ * Redefines the << operator for this class so that we can easily print this to
+ * a file.
+ */
+std::ostream& operator << (std::ostream& os, const Building& b);
+
+/*
+ * Operator: >>
+ * 
+ * Redefines the >> operator for this class so that we can easily load this
+ * object from a stream.
+ */
+std::istream& operator >> (std::istream& os, const Building& b);
 
 #endif
