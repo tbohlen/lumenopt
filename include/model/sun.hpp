7170c107dcd3cae5c90e2bd0c3c197abe14a164c
Turner Bohlen <turnerbohlen@gmail.com>
2014-01-31 17:02:00 -0500

    Implements primitive sun model

--- include/model/sun.hpp
+++ include/model/sun.hpp
@@ -1,7 +1,8 @@
 #ifndef SUN_H
 #define SUN_H
 
-#include "types.h"
+#include "types.hpp"
+#include <vector>
 
 /* 
  * Class Sun
@@ -17,11 +18,47 @@ class Sun {
 	 */
 	Sun(float latitude);
 
+    /*
+     * Constructor: Sun
+     *
+     * Builds the sun and precalculates n directions from which sunlight will
+     * be coming.
+     */
+    Sun(float latitude, int n);
+
 	~Sun();
+
+    /*
+     * Method: build
+     *
+     * Builds the sun model ahead of time to avoid computation on every queery.
+     * Builds the sun model with n individual sample directions. To use these
+     * pre calculated directions the caller must use getDirectionForIndex not
+     * simply getDirection.
+     *
+     * Parameter:
+     * n - the number of sun directions to calculate
+     */
+    void build(int n);
+
+    /*
+     * Method: getNumberOfSamples
+     *
+     * Returns the number of precalculated samples.
+     */
+    int getNumberOfSamples() const;
+
+    /*
+     * Method getDirectionForIndex
+     *
+     * Returns the direction mapped to that index in the precalculated sun
+     * directions vector.
+     */
+    coord getDirectionForIndex(int i) const;
 
 	private:
 		float latitude;
-		
+        std::vector<coord> directions;
 };
 
 #endif
---
+++ src/model/sun.cpp
@@ -0,0 +1,39 @@
+#include "model/sun.hpp"
+#include "helpers.hpp"
+#include <math.h>
+
+#define PI 3.14159265
+
+Sun::Sun(float latitude) : latitude(latitude) {
+
+}
+
+Sun::Sun(float latitude, int n) : latitude(latitude) {
+    this->build(n);
+}
+
+Sun::~Sun() {
+
+}
+
+void Sun::build(int n) {
+    directions.resize(n);
+    int i;
+    for (i = 0; i < n; i++) {
+        coord dir;
+        dir.x = 0;
+        dir.y = -1 * cos((i * PI)/(n-1));
+        dir.z = -1 * sin((i * PI)/(n-1));
+        directions[i] = dir;
+    }
+}
+
+int Sun::getNumberOfSamples() const {
+    return this->directions.size();
+}
+
+coord Sun::getDirectionForIndex(int i) const {
+    myAssert(i >= 0);
+    myAssert(i < this->directions.size());
+    return this->directions[i];
+}
